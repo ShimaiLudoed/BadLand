ad148986c4a48255cf68504df3d9536be0f03dfb
ShimaiLudoed <MikhaiilShvets@yandex.ru>
2024-11-29 12:24:05 +0300

    .

--- BadLand/Assets/_Source/LevelSystem/Level/FallForObstacle.cs
+++ BadLand/Assets/_Source/LevelSystem/Level/FallForObstacle.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System;
 using UnityEngine;
 
 namespace LevelSystem
@@ -7,10 +8,19 @@ namespace LevelSystem
     {
         [SerializeField] private Transform endPoint;
         [SerializeField] private float duration;
+        [SerializeField] private LayerMask ground;
 
         public void Fall()
         {
-            transform.DOMove(endPoint.position, 3);
+            transform.DOMove(endPoint.position, duration);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (LayerMaskCheck.ContainsLayer(ground, other.gameObject.layer))
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
